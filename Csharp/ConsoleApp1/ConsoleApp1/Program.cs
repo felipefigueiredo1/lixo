51f13851bf781423041980bb824ee38038f38ac7
Felipe Figueiredo <Felipe Figueiredo>
2024-02-29 22:07:03 -0300

    Start with C#

---
+++ Csharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1 // namespace define o caminho para encontrarmos essa classe.
+{
+    /*
+     * Em C#, uma "internal class" é uma classe que só é acessível dentro do mesmo assembly 
+     * (ou seja, o mesmo arquivo .dll ou .exe). 
+     * Isso significa que outras classes fora do assembly 
+     * não podem acessar diretamente uma classe interna.
+     */
+    internal class Program
+    {
+        /* O método Main é o ponto de entrada do programa
+         * Ele precisa ser estático porque durante a compilação, o compilador não irá instanciar a classe
+         * Ele apenas irá executar o método Main
+         * Todo programa deve haver apenas 1 método Main
+         */
+        static void Main(string[] args) // Argumentos que são passados na execução do programa.
+        {
+            if(args.Length > 0) {
+                Console.WriteLine(args[0]);
+            }
+
+            /* em C#, assim como na maioria das linguagens, o backslash aciona
+            um caracter especial, que fará algum efeito na string que ele esta inserido,
+            no exemplo abaixo "t" acrescenta varios espaços (tab), e "n" quebra a linha */
+
+            Console.WriteLine("I\t like\t pizza\n!");
+            // Isso é um comentário
+            /* Isso também é um comentário */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Csharp/Variables/Variables/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x; // declarando uma variável do tipo inteira.
+            x = 123; // inciando a variável x inserindo um valor nela.
+
+            int y = 321; // declarando e iniciando uma variável.
+
+            int z = x + y;
+
+            int age = 21;
+
+            double height = 300.5; // variável do tipo double para numeros decimais.
+
+            bool alive = true; // variável do tipo booleana para definição de "true" ou "false".
+
+            char symbol = '@'; // variável do tipo caracter
+            String name = "Felipe"; // variável do tipo string ( aqui é uma classe )
+            /*
+             * Uma String em algumas linguagens é um array de caracteres
+             * e em outras linguagens (C# incluso) é uma classe
+             */
+
+
+            // em c# a concatenação de strings se faz utilizando o simbolo de adição
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("z = " + z);
+            Console.WriteLine("your age is = " + age);
+            Console.WriteLine("your height is = " + height + "cm");
+            Console.WriteLine("are you alive = " + alive);
+            Console.WriteLine("you symbol are = " + symbol);
+            Console.WriteLine("your name are = " + name);
+        }
+    }
+}
